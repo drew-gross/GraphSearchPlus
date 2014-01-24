b46a6c21e7a0106b352735eb1d348d42226c420c
Drew Gross <drew.a.gross@gmail.com>
2014-01-24 18:45:52 -0500

    working app (smoketest)

--- GraphSearchPlus/urls.py
+++ GraphSearchPlus/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'GraphSearchPlus.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+	url(r'^search/', include('GraphSearchApp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
