39c15f59ab3970e8f0ee08fbc84865bc3ae21847
Drew Gross <drew.a.gross@gmail.com>
2014-01-25 09:56:20 -0500

    give up on favicon

--- GraphSearchApp/urls.py
+++ GraphSearchApp/urls.py
@@ -4,5 +4,4 @@ from GraphSearchApp import views
 urlpatterns = patterns('',
     url(r'^login/', 'views.login', name='login'),
     url(r'^$', 'views.main', name='main'),
-    # url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': 'static/assets/favicon.ico'}),
 )
